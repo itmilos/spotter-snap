9f859da5411c71b65ecb08734cf7b35ef09edfd8
itmilos <milosrujevic@gmail.com>
2023-06-06 18:29:55 +0200

    Init

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,5 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { heading, panel, text } from '@metamask/snaps-ui';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -11,23 +11,57 @@ import { panel, text } from '@metamask/snaps-ui';
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
+
+
+const getColor = (result: number) => {
+  switch (result) {
+    case 3:
+      return '🟩';
+    case 2:
+      return '🟧';
+    default:
+      return '🟥';
+  }
+};
+
+
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
       return snap.request({
+        // method: 'snap_dialog',
+        // params: {
+        //   type: 'confirmation',
+        //   content: panel([
+        //     text("You are about to sign contract with " + getColor(3) + " security"),
+        //   ]),
+        // },
         method: 'snap_dialog',
         params: {
-          type: 'confirmation',
+          type: 'prompt',
           content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
+            heading('You are about to singn' + getColor(1) + ' security'),
+            text('Please Confirm by typing YES'),
           ]),
+          placeholder: 'placeholder',
         },
       });
     default:
       throw new Error('Method not found.');
   }
 };
+
+//
+// const contractVerificationScore = await getContractVerificationScore({
+//   chainId,
+//   contractAddress: (transaction as TransactionObject).to,
+// });
+//
+// const overallScore = calculateOverallScoreWithWeight({
+//   contractTransactionCountScore,
+//   contractUserTransactionScore,
+//   contractAgeScore,
+//   contractVerificationScore,
+// });
+//
+//
